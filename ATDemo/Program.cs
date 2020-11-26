9a8a0a2b041232020ddf7f6fdb42407e804cfd3f
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-11-27 00:50:26 +0200

    Add project files.

---
+++ ATDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
